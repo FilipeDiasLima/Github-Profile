c23dcfac440bb78fc61a23b36f4ac5881fd35fc2
Filipe Dias <filipdiasdelima@gmail.com>
2020-08-24 00:59:00 -0400

    functionalities

--- web/src/Pages/Dashboard/index.js
+++ web/src/Pages/Dashboard/index.js
@@ -1,13 +1,62 @@
-import React from 'react';
-import { FiSearch } from 'react-icons/fi'
+import React, { useState, useEffect } from 'react';
+import { FiSearch, FiStar, FiUsers, FiBook, FiChevronRight } from 'react-icons/fi'
+import api from '../../services';
 
-import Profile from '../../components/Profile'
-
+import bolinha from '../../assets/bolinha.png';
+import gitnetwork from '../../assets/git-network.png';
 import gitlogo from '../../assets/github-logo.png';
 
-import { Header, Form } from './styles';
+import { Header, Form, Profile, InfoMain, InfoSecundary, Repositories } from './styles';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
+  const [profile, setProfile] = useState(false);
+  const [newProfile, setNewProfile] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [infoProfile, setInfoProfile] = useState();
+
+  async function handleProfile(e) {
+    e.preventDefault();
+
+    const response = await api.get(`users/${newProfile}`);
+    const responseRepo = await api.get(`users/${newProfile}/repos`);
+
+    const {
+      avatar_url,
+      login,
+      name,
+      html_url,
+      repos_url,
+      bio,
+      public_repos,
+      followers,
+    } = response.data;
+
+    let starCount = 0;
+    let i = 0;
+    for (i = 0; i < responseRepo.data.length; ++i) {
+      starCount += responseRepo.data[i].stargazers_count;
+    }
+
+    const profile = {
+      avatar_url,
+      login,
+      name,
+      html_url,
+      repos_url,
+      bio,
+      public_repos,
+      followers,
+      starCount,
+    }
+
+    setInfoProfile(profile);
+    setProfile(true);
+    setRepositories(responseRepo.data);
+
+  }
+
+
   return (
     <>
       <Header>
@@ -21,15 +70,88 @@ export default function Dashboard() {
         </div>
       </Header>
 
-      <Form>
-        <input placeholder="Search profile..." />
+      <Form onSubmit={handleProfile}>
+        <input
+          value={newProfile}
+          onChange={e => setNewProfile(e.target.value)}
+          placeholder="Search profile..."
+        />
 
         <button type="submit">
           <FiSearch color="#FFF" size={30} />
         </button>
       </Form>
 
-      <Profile />
+      {profile &&
+        <Profile>
+          <InfoMain>
+            <img
+              src={infoProfile.avatar_url}
+              alt={infoProfile.name}
+            />
+            <div>
+              <strong>{infoProfile.name}</strong>
+              <p>{infoProfile.login}</p>
+              <p>{infoProfile.bio}</p>
+            </div>
+            <Link to="/"><button type="button">View on Github</button></Link>
+          </InfoMain>
+
+          <InfoSecundary>
+            <ul className="statusInfo">
+              <li>
+                <FiStar color="#FFF" size={35} />
+                <p>{infoProfile.starCount}</p>
+              </li>
+
+              <li>
+                <FiUsers color="#FFF" size={35} />
+                <p>{infoProfile.followers}</p>
+              </li>
+
+              <li>
+                <FiBook color="#FFF" size={35} />
+                <p>{infoProfile.public_repos} Repositories</p>
+              </li>
+            </ul>
+
+            <strong>Repositories</strong>
+
+            <Repositories>
+              {repositories.map(repository => (
+                <a key={repository.id} href="/">
+                  <div className="infos">
+                    <strong>{repository.full_name}</strong>
+                    <p>{repository.description}</p>
+
+                    <footer>
+                      {repository.language &&
+                        <div>
+                          <img src={bolinha} alt="Language" />
+                          <p>{repository.language}</p>
+                        </div>
+                      }
+
+                      <div>
+                        <FiStar color="#DDFF0C" size={14} />
+                        <p>{repository.stargazers_count}</p>
+                      </div>
+
+                      <div>
+                        <img src={gitnetwork} alt="fork" />
+                        <p>{repository.forks_count}</p>
+                      </div>
+                    </footer>
+                  </div>
+
+                  <FiChevronRight size={30} />
+                </a>
+              ))}
+            </Repositories>
+
+
+          </InfoSecundary>
+        </Profile>}
 
     </>
   )
--- web/src/Pages/Dashboard/styles.js
+++ web/src/Pages/Dashboard/styles.js
@@ -45,7 +45,7 @@ export const Header = styled.div`
   }
 `;
 
-export const Form = styled.div`
+export const Form = styled.form`
   margin-top: 52px;
   width: 408px;
   height: 55px;
@@ -67,5 +67,272 @@ export const Form = styled.div`
     border: none;
     border-radius: 0 8px 8px 0;
   }
+
+  @media screen and (max-width: 1100px){
+    width: 100%;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding: 17px;
+  background: #71777B;
+  border: none;
+  border-radius: 8px;
+  margin-top: 39px;
+
+  @media screen and (max-width: 1100px){
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 60px 25px;
+  }
+`;
+
+export const InfoMain = styled.div`
+  display: flex;
+  width: 320px;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px 26px;
+  border-right: 2px solid white;
+
+  img {
+    width: 250px;
+    height: 250px;
+    border: 4px solid white;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 24px;
+
+    strong {
+      font-style: normal;
+      font-weight: bold;
+      font-size: 26px;
+      line-height: 39px;
+    }
+
+    p {
+      margin-left: 5px;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 27px;
+    }
+
+    p + p {
+      width: 231px;
+      margin-top: 20px;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 21px;
+    }
+  }
+
+  a {
+    button {
+      text-decoration: none;
+      align-self: center;
+      margin-top: 23px;
+      width: 225px;
+      height: 42px;
+      border: none;
+      background: #61A360;
+      border-radius: 8px;
+      font-weight: bold;
+      font-size: 18px;
+      line-height: 27px;
+      transition: background-color 0.3s;
+
+      &:hover {
+        background-color: #24292E;
+        border: 1px solid #38B636;
+      }
+    }
+  }
+
+  @media screen and (max-width: 1100px){
+    border: none;
+    padding: 0;
+    width: 360px;
+
+    img {
+      width: 350px;
+      height: 350px;
+    }
+
+    div {
+      width: 100%;
+      padding: 0;
+      align-items: flex-start;
+      
+      strong {
+        font-size: 28px;
+      }
+
+      p {
+        font-size: 18px;
+        margin: 16px 0;
+      }
+
+      p + p {
+        width: 100%;
+        font-size: 16px;
+        margin: 16px 0;
+      }
+    }
+    
+    button {
+      width: 100%;
+      height: 60px;
+    }
+  }
+`;
+
+export const InfoSecundary = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 670px;
+  padding: 19px 55px 19px 0px;
+
+
+  .statusInfo {
+    display: flex;
+    list-style: none;
+    justify-content: space-between;
+
+    li {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+
+      p {
+        font-weight: normal;
+        font-size: 24px;
+        line-height: 36px;
+        margin-left: 18px;
+        align-items: baseline;
+      }
+    }
+  }
+
+  strong {
+    margin-top: 19px;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 30px;
+  }
+
+  @media screen and (max-width: 1100px){
+    margin-top: 50px;
+    padding: 0;
+    align-items: center;
+    width: 100%;
+
+    .statusInfo {
+      display: block;
+
+      li + li {
+        margin-top: 21px;
+      }
+    }
+    
+    strong {
+      font-size: 24px;
+      font-weight: bolder;
+      margin-top: 52px;
+      margin-bottom: 32px;
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 16px;
+  min-height: 116px;
+  max-width: 670px;
+
+  a {
+    background-color: transparent;
+    border-radius: 8px;
+    width: 100%;
+    padding: 10px 20px;
+    text-decoration: none;
+
+    display: flex;
+    align-items: flex-start;
+    border: 2px solid white;
+    transition: background-color 0.3s, transform 0.2s;
+    margin-bottom: 10px;
+
+    & + a {
+     margin-top: 16px;
+    }
+
+    .infos {
+      display: block;
+      flex: 1;
+
+      strong {
+        color: #FFF;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 24px;
+      }
+
+      p {
+        color: #FFF;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 18px;
+      }
+      
+      footer {
+        display: flex;
+        margin: 20px 0 0 0;
+
+        div {
+          display: flex;
+          align-items: flex-start;
+          font-weight: normal;
+          font-size: 12px;
+          line-height: 18px;
+          color: #FFF;
+          
+          p {
+            margin-left: 10px;
+          }
+
+          & + div {
+            margin-left: 25px;
+          }
+        }
+      }
+    }
+
   
+
+    svg {
+      align-self: center;
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+
+    &:hover {
+      background-color: #24292E;
+      border: 2px solid #38B636;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
 `;
